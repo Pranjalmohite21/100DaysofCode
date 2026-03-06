08aeea9d788965be9eaec9e1327de8ea7b031462
Pranjal Mohite <pranjalmohitevk@gmail.com>
2026-03-06 19:18:53 +0530

    Create Day_55(2).py

---
+++ Day_55(2).py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eng=int(input())
+roll_eng= set(map(int, input().split()))
+french=int(input())
+roll_french= set(map(int, input().split()))
+print(len(roll_eng.union(roll_french)))
