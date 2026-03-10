b659212966cc19e8ebbb07ac8b436e1ef959b927
Pranjal Mohite <pranjalmohitevk@gmail.com>
2026-03-10 18:34:18 +0530

    Create Day_57(2).py

---
+++ Day_57(2).py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+#for i in range(n):
+english=input().split()
+english=set(english)
+b=int(input())
+#for i in range(b):
+french=input().split()
+french=set(french)
+z=len(english.difference(french))
+print(z)
